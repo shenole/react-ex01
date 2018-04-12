a2b301b73929328ff40ed34fbd9b75174cdf45d7
Shenole Latimer <shenole@gmail.com>
2018-04-11 21:56:26 -0400

    added a table to the app component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!!!!</h1>
+        <h1>Test Table</h1>
+        <h3>This is just practice to get used to JSX</h3>
+        <table>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>AGE</th>
+          </tr>
+          <tr>
+            <td>01</td>
+            <td>Mike</td>
+            <td>38</td>
+          </tr>
+        </table>
       </div>
     );
   }
